cd6e5e97b6d8577665b801228501700156cf1052
Tanjina sultana <tanjinasultana388@gmail.com>
2023-10-19 04:17:28 +0600

    delete operation are added in cartColeection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require( 'express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -53,6 +53,13 @@ async function run() {
     res.send(result);
 
    })
+   app.delete('/cart/:id',async(req,res)=>{
+    const id = req.params.id;
+    console.log('delete',id);
+    const query = {_id: id };
+    const result = await cartCollection.deleteOne(query);
+    res.send(result);
+   } )
 
 
 
